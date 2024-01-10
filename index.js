072ab1b77968887247ce4a020834f8043a9d30a4
Izabella-Nascimento <izabellanascimento1606@gmail.com>
2024-01-10 20:48:37 -0300

    Bootstrap linked and question form created

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/",(req, res) => {
+  
+  res.render("index.ejs");
 });
 
-app.listen(8080, () => {
+app.get("/ask", (req, res) => {
+  res.render("ask");
+})
+
+app.listen(3000, () => {
   console.log("App rodando!");
 });
